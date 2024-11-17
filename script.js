f32d964cf3d3ed7d9b952c9c2f3308ddb6757d33
Pedro Henrique <135714305+ssphenrique@users.noreply.github.com>
2024-11-17 20:21:50 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.getElementById("contactForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Impede o envio real do formulário
+    event.preventDefault();
     const statusMessage = document.getElementById("statusMessage");
-    statusMessage.style.display = "block"; // Exibe a mensagem de status
-    statusMessage.textContent = "Envio Concluído!"; // Define o texto da mensagem
+    statusMessage.style.display = "block";
+    statusMessage.textContent = "Envio Concluído!";
 });
